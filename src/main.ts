02217a71d84a3e392a514b0fc6f99ae6d23d60dd
Leandroepk <leandro.kondratzky@gmail.com>
2024-07-18 10:07:24 -0300

    port-handler

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     new FastifyAdapter({ logger: false }),
   );
   app.enableCors();
-  const port = process.env.port || 3000;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
 }
 bootstrap();

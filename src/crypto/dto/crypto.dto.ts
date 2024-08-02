1cf33caf21b3b666682f72030d50f515c1279594
Leandroepk <leandro.kondratzky@gmail.com>
2024-08-02 11:24:29 -0300

    fix

--- src/crypto/dto/crypto.dto.ts
+++ src/crypto/dto/crypto.dto.ts
@@ -1,6 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CryptoDto {
+  @ApiProperty()
   N: number;
+
+  @ApiProperty()
   T: number;
+
+  @ApiProperty()
   D: number;
+
+  @ApiProperty()
   SEED: number;
 }

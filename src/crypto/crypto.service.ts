253597f5a55b597b84d1704077464ed3a24577b7
Leandroepk <leandro.kondratzky@gmail.com>
2024-08-02 15:19:23 -0300

    v1

--- src/crypto/crypto.service.ts
+++ src/crypto/crypto.service.ts
@@ -1,29 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CryptoDto } from './dto/crypto.dto';
+import Decimal from 'decimal.js';
 
 @Injectable()
 export class CryptoService {
   crypto(createCryptoDto: CryptoDto) {
-    console.log(createCryptoDto);
-    const { N, T, D, SEED } = createCryptoDto;
-    const TID = SEED + (T - 1);
-    const dni = TID / Math.pow(10, 14);
-    let x = 0.444666 + dni;
-    const r = 3.611 + dni * Math.pow(10, 3);
+    Decimal.config({ precision: 200 });
+    const { N, T = 1, D, SEED } = createCryptoDto;
+
+    const TID = new Decimal(SEED + (T - 1));
+    const dni = TID.dividedBy(new Decimal(10).pow(14));
+    let x = dni.plus(0.444666);
+    const r = dni.mul(new Decimal(10).pow(3)).plus(3.611);
     const K2 = [];
     const Mk2 = [];
 
     for (let n = 0; n < N; n++) {
-      x = r * x * (1 - x);
-      let acuy = 0;
-      let acuk = 0;
+      x = r.mul(x).mul(x.minus(1));
+      let acuy = new Decimal(0);
+      let acuk = new Decimal(0);
       const k2 = [];
 
       for (let c = 1; c <= D; c++) {
-        const y = Math.floor(x * Math.pow(10, c)) - acuy * 10;
-        acuy += y;
-        k2[c - 1] = y % 2 === 0 ? 0 : 1;
-        acuk += k2[c - 1] * Math.pow(2, c - 1);
+        const a = new Decimal(10).pow(c);
+        let y = x.mul(a);
+        const y2 = y.floor();
+        const y3 = acuy.mul(10);
+        y = y2.minus(y3);
+        acuy = acuy.plus(y);
+
+        k2[c - 1] = +y.toString().split('.')[0] % 2 ? 0 : 1;
+        console.log({ y, y2, y3 });
+        acuk = acuk.plus(new Decimal(k2[c - 1]).mul(new Decimal(2).pow(c - 1)));
+        console.log(acuk);
       }
 
       Mk2[n] = k2; // Matriz de claves en estado binario

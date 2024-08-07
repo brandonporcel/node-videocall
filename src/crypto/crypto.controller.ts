ba940154b7d049454ec177d4250f9b9d1e203c9c
Leandroepk <leandro.kondratzky@gmail.com>
2024-08-07 14:32:38 -0300

    v2

--- src/crypto/crypto.controller.ts
+++ src/crypto/crypto.controller.ts
@@ -9,8 +9,8 @@ export class CryptoController {
 
   @Post('')
   @ApiExcludeEndpoint()
-  crypto(@Body() a: CryptoDto) {
+  getKeys(@Body() a: CryptoDto) {
     console.log(a);
-    return this.cryptoService.crypto(a);
+    return this.cryptoService.getKeys(a);
   }
 }
